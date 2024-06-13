81c487727be51aa923c115d78cac60345fd29aa1
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-06-13 14:25:15 -0300

    Revs code

--- js/main.js
+++ js/main.js
@@ -13,19 +13,19 @@ function Carrossel (){
 }
 setInterval(Carrossel,1999)
 
-let items =  document.querySelectorAll(".BOX  .BOX1");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
+// let items =  document.querySelectorAll(".BOX  .BOX1");
+// let next = document.getElementById("next");
+// let prev = document.getElementById("prev");
 
-let active = 3;
+// let active = 3;
 
-function LoadShow(){
-    let stt = 0 ;
+// function LoadShow(){
+//     let stt = 0 ;
 
-    items[active].transform = `none`;
-    items[active].zIndex = 1;
-    items[active].filter = `blur(5px)`
-    items[active].opacity =stt > 2 ? 
-}
+//     items[active].transform = `none`;
+//     items[active].zIndex = 1;
+//     items[active].filter = `blur(5px)`
+//     items[active].opacity =stt > 2 ? 
+// }
 
 
