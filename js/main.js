9cc4cecbd56f553fe5ef65a102e4ace511fb604f
Gustavo <gustavomirandaoliveira@outlook.com>
2024-06-14 01:31:32 -0300

    Navbar adicionado

--- js/main.js
+++ js/main.js
@@ -1,31 +1,10 @@
-const imgs = document.getElementById("img")
-const img = document.querySelectorAll("#img img")
-
-let idx = 0;
-
-function Carrossel (){
-    idx ++;
-    if(idx > img.length -1){
-        idx = 0;
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "./img/menu_white_36dp.svg"
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "./img/close_white_36dp.svg"
     }
-
-    imgs.style.transform = `translateX(${-idx * 700}px)`
 }
-setInterval(Carrossel,1999)
-
-// let items =  document.querySelectorAll(".BOX  .BOX1");
-// let next = document.getElementById("next");
-// let prev = document.getElementById("prev");
-
-// let active = 3;
-
-// function LoadShow(){
-//     let stt = 0 ;
-
-//     items[active].transform = `none`;
-//     items[active].zIndex = 1;
-//     items[active].filter = `blur(5px)`
-//     items[active].opacity =stt > 2 ? 
-// }
-
-
