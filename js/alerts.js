b0c197aa15ed910ca83df0a7ef8197f25eef1831
Gustavo <gustavomirandaoliveira@outlook.com>
2024-06-15 06:04:29 -0300

    Rascunhos html

---
+++ js/alerts.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    alert('Obrigado pelo cadastro!');
+    alert('Parabéns, você está cadastrado!');
+    window.location.href = 'endereco.html'; // Redireciona para endereco.html após os alertas
+}
