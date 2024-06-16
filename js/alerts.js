b0bef5b8704522de16e5c71e218989c7700bfcb0
Gustavo <gustavomirandaoliveira@outlook.com>
2024-06-16 01:23:54 -0300

    html css js cartão feito

--- js/alerts.js
+++
@@ -1,5 +0,0 @@
-function mostrarMensagem() {
-    alert('Obrigado pelo cadastro!');
-    alert('Parabéns, você está cadastrado!');
-    window.location.href = 'endereco.html'; // Redireciona para endereco.html após os alertas
-}
