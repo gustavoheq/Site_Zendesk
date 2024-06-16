bfc735d5b8f0d3319b77181b8e4ee65213ddc6bc
Gustavo <gustavomirandaoliveira@outlook.com>
2024-06-16 18:25:37 -0300

    Estrutura html

--- js/main.js
+++ js/main.js
@@ -149,3 +149,18 @@ const initApp = () => {
         })
 }
 initApp();
+
+const imgs = document.getElementById("img")
+const img = document.querySelectorAll("#img img")
+
+let idx = 0;
+
+function Carrossel() {
+    idx++;
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 700}px)`
+}
+setInterval(Carrossel, 2000)
