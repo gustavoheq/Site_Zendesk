87802da90f6140f0e63fdb969e42d7bc04c109a5
Gustavo <gustavomirandaoliveira@outlook.com>
2024-06-16 22:49:34 -0300

    Adicionado nomes

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ const addDataToHTML = () => {
                 `<img src="${product.image}" alt="">
                 <h2>${product.name}</h2>
                 <div class="price">$${product.price}</div>
-                <button class="addCart">Add To Cart</button>`;
+                <button class="addCart">Adicione ao Carrinho</button>`;
             listProductHTML.appendChild(newProduct);
         });
     }
